0c629f79d5061b38c1666f3dae72e79574c61e7a
bang <kzo12341@gmail.com>
2025-04-30 22:07:09 +0900

    feat: 가계부 업데이트

--- src/main/java/com/project/abook/extracker/controller/ExTrackerRestController.java
+++ src/main/java/com/project/abook/extracker/controller/ExTrackerRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,4 +44,11 @@ public class ExTrackerRestController implements ExTrackerRestControllerDocs {
         return ResponseEntity.ok(exTrackers);
     }
 
+    @PutMapping("/exTracker/{id}")
+    public ResponseEntity<Void> updateExTracker(@RequestBody ExTracker exTracker) {
+
+        exTrackerService.update(exTracker);
+
+        return ResponseEntity.noContent().build();
+    }
 }

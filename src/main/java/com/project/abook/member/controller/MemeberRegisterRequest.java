e4585c98f910c71ee2221460e69139fa9c3a0947
bang <kzo12341@gmail.com>
2025-05-06 14:17:43 +0900

    feat: add MemberRegisterRequest

--- src/main/java/com/project/abook/member/controller/MemeberRegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.project.abook.member.controller;
-
-import lombok.Getter;
-
-@Getter
-public class MemeberRegisterRequest {
-
-
-}
---
+++ src/main/java/com/project/abook/member/dto/MemeberRegisterRequest.java
@@ -0,0 +1,12 @@
+package com.project.abook.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemeberRegisterRequest {
+
+    private String username;
+    private String password;
+    private String email;
+    
+}

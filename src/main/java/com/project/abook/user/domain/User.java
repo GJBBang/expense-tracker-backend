97291ea7b0fdf2724c3c6b344f32605473060b2e
bang <kzo12341@gmail.com>
2025-05-06 13:08:42 +0900

    feat: add builder constructor to User and apply BaseEntity

--- src/main/java/com/project/abook/user/domain/User.java
+++ src/main/java/com/project/abook/user/domain/User.java
@@ -1,24 +1,38 @@
 package com.project.abook.user.domain;
 
+import com.project.abook.global.domain.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "username", length = 20, nullable = false)
     private String username;
 
+    @Column(name = "email", length = 50, nullable = false)
     private String email;
 
+    @Column(name = "password", length = 255, nullable = false)
     private String password;
 
-    private LocalDateTime createdAt;
-
+    @Builder
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }

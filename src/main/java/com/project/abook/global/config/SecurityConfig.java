4ee2b61d8ab1db86b85160d7efc4b22c739895a0
bang <kzo12341@gmail.com>
2025-05-07 22:47:55 +0900

    feat: add SecurityConfig

--- src/main/java/com/project/abook/global/config/SecurityConfig.java
+++ src/main/java/com/project/abook/global/config/SecurityConfig.java
@@ -1,7 +1,31 @@
 package com.project.abook.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+//        encoders.put("pbkdf2", new Pbkdf2PasswordEncoder("", 200000, 256, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
+//        encoders.put("argon2", new Argon2PasswordEncoder(16, 32, 1, 65536, 3));
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
 }

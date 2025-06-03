9241e521b3f8770f535f1da6bfc0dc7bfaf66736
bang <kzo12341@gmail.com>
2025-06-03 21:19:00 +0900

    feat: 토큰 생성 메서드 추가

--- src/main/java/com/project/abook/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/com/project/abook/auth/infrastructure/JwtTokenProvider.java
@@ -1,31 +1,61 @@
 package com.project.abook.auth.infrastructure;
 
+import com.project.abook.auth.domain.Authority;
+import com.project.abook.auth.dto.TokenResponse;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
 
+    private static final String JWT_HEADER_PARAM_TYPE = "typ";
+    private static final String JWT_PAYLOAD_AUTHORITY_TYPE = "auth";
+
+    private final Key key;
     private final String headerType;
     private final String issuer;
-    private final Long accessTime;
-    private final Long refreshTime;
+    private final long accessTime;
+    private final long refreshTime;
 
-    public JwtTokenProvider(
-            @Value("${jwt.token.header-type}") String headerType,
-            @Value("${jwt.token.issuer") String issuer,
-            @Value("${jwt.token.access-time") Long accessTime,
-            @Value("${jwt.token.refesh-time") Long refeshTime
-    ) {
+    public JwtTokenProvider(@Value("${jwt.token.header-type}") String headerType,
+                            @Value("${jwt.token.issuer}") String issuer,
+                            @Value("${jwt.token.secret}") String secret,
+                            @Value("${jwt.token.access-time}") long accessTime,
+                            @Value("${jwt.token.refresh-time}") long refreshTime) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
         this.headerType = headerType;
         this.issuer = issuer;
         this.accessTime = accessTime;
-        this.refreshTime = refeshTime;
+        this.refreshTime = refreshTime;
     }
 
-//    public String generateAccessToken(String subject) {
-//        return Jwts.builder()
-//                .
-//    }
+    public TokenResponse createToken(String subject, Authority authority) {
+        String accessToken = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setHeaderParam(JWT_HEADER_PARAM_TYPE, headerType)
+                .setSubject(subject)
+                .claim(JWT_PAYLOAD_AUTHORITY_TYPE, authority.getAuthorityCode())
+                .setIssuer(issuer)
+                .setExpiration(new Date((new Date()).getTime() + accessTime))
+                .setIssuedAt(new Date())
+                .compact();
+
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(new Date().getTime() + refreshTime))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
--- src/main/java/com/project/abook/global/exception/ErrorCode.java
+++ src/main/java/com/project/abook/global/exception/ErrorCode.java
@@ -7,6 +7,11 @@ public enum ErrorCode {
     // Global
     GLOBAL_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다. 관리자에게 문의해주세요.", "GL01"),
 
+    // authority
+    AUTHORITY_NOT_FOUND_BY_AUTHORITY_CODE(HttpStatus.NOT_FOUND, "존재하지 않는 권한코드입니다.", "A01"),
+    AUTHORITY_ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "접근 권한이 없는 권한코드입니다.", "A02"),
+    AUTHORITY_ENTRY_POINT(HttpStatus.UNAUTHORIZED, "오류가 있는 권한코드입니다.", "A03"),
+
     ;
 
     private final HttpStatus httpStatus;

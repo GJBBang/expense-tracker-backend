93c47dca03976b6016b988002a35a1f07234ac0e
bang <kzo12341@gmail.com>
2025-06-03 13:49:51 +0900

    feat: 커스텀예외 클래스 추가

--- src/main/java/com/project/abook/global/exception/CustomException.java
+++ src/main/java/com/project/abook/global/exception/CustomException.java
@@ -5,7 +5,11 @@ public class CustomException extends RuntimeException {
     private ErrorCode errorCode;
 
     private CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
 }

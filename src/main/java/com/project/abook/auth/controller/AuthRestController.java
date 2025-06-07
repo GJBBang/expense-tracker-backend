6511c7368c76c4b4d7f90c6857eb75a3ffb3c04b
bang <kzo12341@gmail.com>
2025-06-07 22:24:13 +0900

    feat: 로그인 dto 추가

--- src/main/java/com/project/abook/auth/controller/AuthRestController.java
+++ src/main/java/com/project/abook/auth/controller/AuthRestController.java
@@ -1,16 +1,23 @@
 package com.project.abook.auth.controller;
 
+import com.project.abook.auth.dto.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/api/v1")
 public class AuthRestController {
 
     @PostMapping("/members/login")
-    public ResponseEntity<Void> login() {
+    public ResponseEntity<Void> login(LoginRequest request) {
+
+        log.info("request Name : {}", request.getMemberName());
+        log.info("request Password : {}", request.getPassword());
+
         return ResponseEntity.noContent().build();
     }
 }

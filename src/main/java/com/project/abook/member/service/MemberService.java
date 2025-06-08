0a61d0f2b57aecc09ddd3042fb0216d1c8c63572
bang <kzo12341@gmail.com>
2025-06-08 13:02:59 +0900

    refactor: 서비스 인터페이스 삭제

--- src/main/java/com/project/abook/member/service/MemberService.java
+++ src/main/java/com/project/abook/member/service/MemberService.java
@@ -1,8 +1,37 @@
 package com.project.abook.member.service;
 
+import com.project.abook.global.exception.BusinessException;
+import com.project.abook.global.exception.ErrorCode;
+import com.project.abook.member.domain.Member;
 import com.project.abook.member.dto.MemberRegisterRequest;
+import com.project.abook.member.mapper.MemberMapper;
+import com.project.abook.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
 
-    Long save(MemberRegisterRequest request);
+    private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Long save(MemberRegisterRequest request) {
+
+        Member member = memberMapper.toMember(request);
+        member.encryptPassword(passwordEncoder);
+
+        return memberRepository.save(member).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findByMemberName(String memberName) {
+        return memberRepository.findByMemberName(memberName)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND_BY_MEMBER_NAME));
+    }
 }

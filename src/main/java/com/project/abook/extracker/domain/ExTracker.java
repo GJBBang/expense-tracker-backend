79d6596f552c7e8f6b7fecff89dfb52887910cc8
bang <kzo12341@gmail.com>
2025-07-05 14:22:46 +0900

    feat: 가계부 멤버 연관관계 추가

--- src/main/java/com/project/abook/extracker/domain/ExTracker.java
+++ src/main/java/com/project/abook/extracker/domain/ExTracker.java
@@ -1,14 +1,18 @@
 package com.project.abook.extracker.domain;
 
 import com.project.abook.global.domain.BaseEntity;
+import com.project.abook.member.domain.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -38,4 +42,12 @@ public class ExTracker extends BaseEntity {
     private PaymentMethod paymentMethod;    // 결제 수단
 
     private String memo;                 // 추가 메모
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memeber_id", nullable = false)
+    private Member member;
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
